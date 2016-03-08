c53efab6aaa01419146e0b7c2678c53f6e44a8bf
joelhooks <joelhooks@gmail.com>
2016-03-08 14:35:17 -0500

    add filtering and sorting

--- ng-bootcamp/08_filter_and_order_by/app.js
+++ ng-bootcamp/08_filter_and_order_by/app.js
@@ -2,15 +2,6 @@ angular.module('exampleApp', [])
   .controller('ExampleCtrl', function ($http) {
     var example = this;
 
-    example.sortField = 'name';
-
-    example.updateSortField = function(sortField) {
-      if(sortField === example.sortField) {
-        sortField = '-' + sortField;
-      }
-      example.sortField = sortField;
-    };
-
     $http.get('http://www.json-generator.com/api/json/get/cnORtoBisy')
       .then(function successHandler(result) {
         example.people = result.data;
