8e3e759836c36359e1eadfc1b7f449ed84a77987
joelhooks <joelhooks@gmail.com>
2016-03-09 09:41:53 -0500

    create a tab component :fire:

---
+++ ng-bootcamp/16_complex_component/app.js
@@ -0,0 +1,50 @@
+angular.module('exampleApp', [])
+  .controller('ExampleCtrl', function () {
+    var example = this;
+    example.message = "This is a message."
+  })
+
+  .directive('tabs', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      templateUrl: 'tabs.html',
+      controller: 'TabsCtrl as tabs'
+    }; // Directive Definition Object DDO
+  })
+  .controller('TabsCtrl', function() {
+    var tabs = this;
+
+    tabs.registeredTabs = [];
+    tabs.selectedTab = null;
+
+    tabs.selectTab = function(tab) {
+      tabs.selectedTab.selected = false;
+      tab.selected = true;
+      tabs.selectedTab = tab;
+    };
+
+    tabs.registerTab = function(tab) {
+      if(!tabs.selectedTab) {
+        tab.selected = true;
+        tabs.selectedTab = tab;
+      }
+      tabs.registeredTabs.push(tab);
+    }
+  })
+  .directive('tab', function () {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        label: '@'
+      },
+      require: '^tabs',
+      template: '<div ng-if="selected" ng-transclude></div>',
+      link: function(scope, element, attrs, tabsCtrl) {
+        tabsCtrl.registerTab(scope);
+      }
+    }; // Directive Definition Object DDO
+  })
+;
